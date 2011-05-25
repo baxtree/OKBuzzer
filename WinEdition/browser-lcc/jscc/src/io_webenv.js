bd31dbd9b6504ec7f83f01e42b23fe528db53ae8
baxtree <baxtree@163.com>
2011-05-25 21:35:12 +0100

    bundles with and without JRE

--- WinEdition/browser-lcc/jscc/src/io_webenv.js
+++
@@ -1,1 +0,0 @@
-//This file remains empty.
--- WinEdition/start.bat
+++
@@ -1,4 +0,0 @@
-call jre\bin\java -jar OKBuzzer.jar 
-sleep 3
-call echo "OKBuzzer is running at http://127.0.0.1:%1"
-call FirefoxPortable_4.0.1_English.paf.exe -fullscreen http://127.0.0.1:%1/
